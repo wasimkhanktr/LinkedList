6e43d1d6de0168415c6f38b163e9d75d55b8037b
MdWasimAkram <wasimkhanktr@gmail.com>
2022-03-30 21:36:55 +0530

    [Wasim] Add: create Linklist

---
+++ LinkedList/LinkListOperation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class LinkListOperation
+    {
+        Node head = null;
+        public void insert(int value)
+        {
+            Node temp = head;
+            Node newNode = new Node();
+            newNode.data = value;
+            newNode.next = null;
+            if (head != null)
+            {
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = newNode;
+            }
+            else
+            {
+                head = newNode;
+            }
+        }
+        public void display()
+        {
+            Node temp = head;
+            while (temp.next != null)
+            {
+                Console.WriteLine(temp.data);
+                temp = temp.next;
+            }
+            Console.WriteLine(temp.data);
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkListOperation list = new LinkListOperation();
+            list.insert(56);
+            list.insert(30);
+            list.insert(70);
+            list.display();
+        }
+    }
+}
