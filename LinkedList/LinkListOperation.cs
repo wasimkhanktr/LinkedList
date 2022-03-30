0d70db65cab29dd0ce40e19401c4bef0db696a99
MdWasimAkram <wasimkhanktr@gmail.com>
2022-03-30 23:09:13 +0530

    [Wasim] Add: specific Node

--- LinkedList/LinkListOperation.cs
+++ LinkedList/LinkListOperation.cs
@@ -8,70 +8,83 @@ namespace LinkedList
 {
     public class LinkListOperation
     {
-        public Node Head;
-        public Node Tail;
-        public LinkListOperation()
-        {
-            Head = null;
-            Tail = null;
-        }
-        public void AddNode(Node node)
-        {
-            if (Head == null && Tail == null)
-            {
-                Head = node;
-                Tail = node;
-            }
-            else
-            {
-                node.next = Head;
-                Head = node;
-            }
-        }
-        internal void Display()
-        {
-            Node temp = Head;
-            if (temp == null)
-            {
-                Console.WriteLine("LinkList is empty");
-            }
-            else
-            {
-                Console.WriteLine("LinkList as follows: ");
-            }
-            while (temp != null)
-            {
-                Console.Write(temp.data);
-                if (temp.next != null)
-                {
-                    Console.Write("->"); ;
-                }
-                temp = temp.next;
-            }
-        }
-        public void DeleteNodeAtFirst()
-        {
-            if (this.Head == null)
-            {
-                Console.WriteLine("Nothig to delete");
-            }
-            Node temp = this.Head;
-            this.Head = this.Head.next;
-            Console.WriteLine("\nRemoved first node from LinkList");
-        }
 
-        public bool search(int data)
-        {
-            Node temp = this.Head;
-            while (temp != null)
+            public Node Head;
+            public Node Tail;
+             public LinkListOperation()
             {
-                if (temp.data == data)
-                {
-                    return true;
-                }
-                temp = temp.next;
+                Head = null;
+                Tail = null;
             }
-            return false;
+            public void AddNode(Node node)
+            {
+                if (Head == null && Tail == null)
+                {
+                    Head = node;
+                    Tail = node;
+                }
+                else
+                {
+                    node.next = Head;
+                    Head = node;
+                }
+            }
+            internal void Display()
+            {
+                Node temp = Head;
+                if (temp == null)
+                {
+                    Console.WriteLine("LinkList is empty");
+                }
+                else
+                {
+                    Console.WriteLine("LinkList as follows: ");
+                }
+                while (temp != null)
+                {
+                    Console.Write(temp.data);
+                    if (temp.next != null)
+                    {
+                        Console.Write("->");
+                    }
+                    temp = temp.next;
+                }
+            }
+            public void DeleteNodeAtFirst()
+            {
+                if (this.Head == null)
+                {
+                    Console.WriteLine("Nothig to delete");
+                }
+                Node temp = this.Head;
+                this.Head = this.Head.next;
+                Console.WriteLine("\nRemoved first node from LinkList");
+            }
+            public bool search(int data)
+            {
+                Node temp = this.Head;
+                while (temp != null)
+                {
+                    if (temp.data == data)
+                    {
+                        return true;
+                    }
+                    temp = temp.next;
+                }
+                return false;
+            }
+            public void AddSpicificNode(int data, Node node)
+            {
+                Node temp = Head;
+                while (temp != null)
+                {
+                    if (temp.data == data)
+                    {
+                        node.next = temp.next;
+                        temp.next = node;
+                    }
+                    temp = temp.next;
+                }
+            }
         }
     }
-}
--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -7,35 +7,40 @@ namespace LinkedList
     {
         static void Main()
         {
-            LinkListOperation linklist = new LinkListOperation();
-            
-            Node node1 = new Node(21);
-            Node node2 = new Node(22);
-            Node node3 = new Node(23);
-            Node node4 = new Node(24);
-            Node node5 = new Node(25);
+                LinkListOperation linklist = new LinkListOperation();
+                Node node1 = new Node(21);
+                Node node2 = new Node(22);
+                Node node3 = new Node(23);
+                Node node4 = new Node(24);
+                Node node5 = new Node(25);
 
-            linklist.AddNode(node1);
-            linklist.AddNode(node2);
-            linklist.AddNode(node3);
-            linklist.AddNode(node4);
-            linklist.AddNode(node5);
+                linklist.AddNode(node1);
+                linklist.AddNode(node2);
+                linklist.AddNode(node3);
+                linklist.AddNode(node4);
+                linklist.AddNode(node5);
 
-            linklist.Display();
-            linklist.DeleteNodeAtFirst();
-            linklist.Display();
+                linklist.Display();
+                linklist.DeleteNodeAtFirst();
+                linklist.Display();
 
-            Console.WriteLine("\nEnter number you want to search");
-            int data = int.Parse(Console.ReadLine());
-            bool res = linklist.search(data);
-            if (res == true)
-            {
-                Console.WriteLine("Data found");
-            }
-            else
-            {
-                Console.WriteLine("Data not found");
+                Console.WriteLine("\nEnter number you want to search");
+                int data = int.Parse(Console.ReadLine());
+                bool res = linklist.search(data);
+                if (res == true)
+                {
+                    Console.WriteLine("Data found");
+                }
+                else
+                {
+                    Console.WriteLine("Data not found");
+                }
+
+                Node node7 = new Node(100);
+                Console.WriteLine("\nEnter a number after you want to add: ");
+                int Data = int.Parse(Console.ReadLine());
+                linklist.AddSpicificNode(Data, node7);
+                linklist.Display();
             }
         }
     }
-}
